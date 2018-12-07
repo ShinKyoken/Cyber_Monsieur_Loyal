a3a0134b997dc53c327e47de5d0f5743f060c61e
Antonin PANDION <antoninpandion28@gmail.com>
2018-12-07 16:29:58 +0100

    Création de la base de données en SQLalchemy

--- appli/models.py
+++ appli/models.py
@@ -1,11 +1,63 @@
 from .app import db
 
+class ADMIN(db.Model):
+    idAdmin        = db.Column(db.Integer, primary_key = True)
+    nomAdmin       = db.Column(db.String(100))
+    prenomAdmin    = db.Column(db.String(100))
+    dateNaissAdmin = db.Column(db.Date)
+    mdpAdmin       = db.Column(db.String(100))
 
-def get_sample():
-    return Book.query.limit(10).all()
+class TOURNOI(db.Model):
+    idT              = db.Column(db.Integer, primary_key = True)
+    idAdmin          = db.Column(db.Integer, db.ForeignKey("ADMIN.idAdmin"))
+    regleT           = db.Column(db.String(100))
+    dateT            = db.Column(db.Date)
+    dureeT           = db.Column(db.String(5))
+    intituleT        = db.Column(db.String(30))
+    descT            = db.Column(db.String(30))
+    typeT            = db.Column(db.String(30))
+    etatT            = db.Column(db.Integer)
+    nbEquipe         = db.Column(db.Integer)
+    nbParicipantsMax = db.Column(db.Integer)
+    disciplineT      = db.Column(db.String(30))
+    lieuT            = db.Column(db.String(30))
 
-def get_all():
-    return Book.query.all()
+class PARTICIPANT(db.Model):
+    idP     = db.Column(db.Integer, primary_key = True)
+    nomP    = db.Column(db.String(100))
+    prenomP = db.Column(db.String(100))
+    mailP   = db.Column(db.String(100))
 
-def get_author(id):
-    return Author.query.get(id)
+class EQUIPE(db.Model):
+    idE           = db.Column(db.Integer, primary_key = True)
+    etatE         = db.Column(db.Integer)
+    nbParticipant = db.Column(db.Integer)
+    idChefE       = db.Column(db.Integer, db.ForeignKey("PARTICIPANT.idP"), unique=True)
+    nomE          = db.Column(db.String(100))
+
+class PHOTO(db.Model):
+    idPhoto   = db.Column(db.Integer, primary_key = True)
+    Photo     = db.Column(db.String(100))
+    descPhoto = db.Column(db.String(100))
+    datePhoto = db.Column(db.Date)
+
+class CONSTITUER(db.Model):
+    idP = db.Column(db.Integer, db.ForeignKey("PARTICIPANT.idP"),primary_key=True)
+    idE = db.Column(db.Integer, db.ForeignKey("EQUIPE.idE"), primary_key=True)
+
+class APPARTENIR(db.Model):
+    idPhoto = db.Column(db.Integer, db.ForeignKey("PHOTO.idPhoto"),primary_key=True)
+    idT     = db.Column(db.Integer, db.ForeignKey("TOURNOI.idT"), primary_key=True)
+
+class INSCRIRE(db.Model):
+    idT = db.Column(db.Integer, db.ForeignKey("TOURNOI.idT"), primary_key=True)
+    idE = db.Column(db.Integer, db.ForeignKey("EQUIPE.idE"),primary_key=True)
+
+class PARTIE(db.Model):
+    idPartie = db.Column(db.Integer, primary_key=True)
+    carteParie = db.Column(db.String(100))
+
+class PARTICIPERPARTIE(db.Model):
+    idE = db.Column(db.Integer, db.ForeignKey("EQUIPE.idE"),primary_key=True)
+    idPartie = db.Column(db.Integer, db.ForeignKey("PARTIE.idPartie"),primary_key=True)
+    idT = db.Column(db.Integer, db.ForeignKey("TOURNOI.idT"),primary_key=True)
