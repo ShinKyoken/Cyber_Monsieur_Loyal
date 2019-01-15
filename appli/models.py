58f4ed35e05161d93de064949b8a6efbc6fe62f8
Antonin PANDION <antoninpandion28@gmail.com>
2019-01-15 17:33:39 +0100

    Correction

--- appli/models.py
+++ appli/models.py
@@ -34,8 +34,8 @@ class PARTICIPANT(db.Model):
     mailP   = db.Column(db.String(100))
 
 class EQUIPE(db.Model):
-    idE           = db.Column(db.Integer, primary_key = True)
-    idT           = db.Column(db.Integer,db.ForeignKey("TOURNOI.idT"),primary_key = True)
+    idE           = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    idT           = db.Column(db.Integer,db.ForeignKey("TOURNOI.idT"),primary_key = True, autoincrement=False)
     etatE         = db.Column(db.Integer)
     points        = db.Column(db.Integer)
     nbParticipant = db.Column(db.Integer)
@@ -231,7 +231,7 @@ def delete_equipe(idEquipe):
 
 def get_participant_by_id(idP):
     return TOURNOI.query.filter_by(idP = idP)
-    
+
 def get_membres_constituer(idEquipe):
     return CONSTITUER.query.filter_by(idE = idEquipe)
 
