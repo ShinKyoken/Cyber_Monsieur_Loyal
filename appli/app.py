95f71d16f9d04a1cb4a4b4f0e3ab865d217638c5
Hugo Pauthier <hugo.pauthier@gmail.com>
2019-03-15 19:55:19 +0100

    erreur de fusion

--- appli/app.py
+++ appli/app.py
@@ -12,6 +12,6 @@ app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 from flask_sqlalchemy import *
 import pymysql
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+pymysql://pandion:pandion@servinfo-mariadb/DBpandion')
+app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+pymysql://root:pauthier@localhost/test')
 db=SQLAlchemy(app)
 app.config['SECRET_KEY'] = "3ec22ed8-06fd-455e-8132-94b9fc65ba51"
