3de897b5791a7ebf0d7134595eb19a226f005821
Hugo Pauthier <hugo.pauthier@etu.univ-orleans.fr>
2019-03-18 09:56:13 +0100

    modif pour le tournoi

--- appli/views.py
+++ appli/views.py
@@ -57,6 +57,7 @@ def resultatMatch(tournoi, partie):
     return render_template("resultatMatch.html",
                            dico_resultat = resultat,
                            partie = partie,
+                           equipes = get_equipe_by_partie(partie),
                            tournoi = tournoi)
 
 
@@ -276,12 +277,21 @@ def voirMatchs(tournoi):
 
     Redirige vers une page affichant les different matchs du tounoi ainsi qu'un classement.
     """
+    # d = {}
+    # equipes_parties_terminees = []
+    # partiesFinies = get_All_Parties_Terminees(tournoi),
+    # for (partie in partiesFinies):
+    #     d[partie] = []
+    #     for equipe in get_equipe_by_partie(partie):
+    #         d[partie].append(equipe)
+
     return render_template(
         "voirMatchs.html",
         listeMaps = get_All_Maps(tournoi),
         tournoi = get_Tournoi_by_id(tournoi),
         equipes = get_All_Equipes_Classe(tournoi),
         equipes2 = get_All_Equipe_by_partie(get_All_partie_by_tournoi(tournoi)),
+        # dico_PartieTerminees_Equipes = d,
         route="tableau"
         )
 
@@ -618,6 +628,7 @@ def valider_modification_equipe(tournoi, equipe):
         dico_participant['prenomP'] = request.form['prenom_membre'+str(i)]
         dico_participant['mailP'] = request.form['mail_membre'+str(i)]
 
+        e.nomE = request.form["nomEquipe"]
         e.commandShell = request.form["commandShell"]
         update_Equipe(e,e.idE)
         update_participant(dico_participant, l[i].idP)
