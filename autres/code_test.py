3e4c6f6fe9138d78721606b89694a65f291a5585
Antonin PANDION <antoninpandion28@gmail.com>
2019-03-19 16:23:20 +0100

    Dossiers pour les 2 anciens fichiers

---
+++ autres/code_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+import random
+import json
+
+
+with open("parametres.json") as json_file:
+    parametres = json.load(json_file)
+
+noms_equipes = list(parametres["equipes"].keys())
+idPartie = parametres["parametres"]["idPartie"]
+
+random.seed()
+random.shuffle(noms_equipes)
+
+scores ={ "equipes" :
+         { equipe : pos for pos, equipe in enumerate(noms_equipes)},
+         "idPartie" : idPartie
+        }
+
+json.dump(scores, sys.stdout, indent=4)
